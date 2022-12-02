87e80b03409d19622f71fbf32560b2ac91977dd2
Peter Porfy <peter.porfy@gmail.com>
2022-12-02 14:01:05 +0100

    01/2

--- 01/index.js
+++ 01/index.js
@@ -2,17 +2,20 @@ import { open } from 'node:fs/promises';
 
 const file = await open('./data');
 
+const calories = [];
 let currentElfCalories = 0;
-let maxCalories = 0;
 
 for await (const line of file.readLines()) {
   if (line.trim() === '') {
-    maxCalories = Math.max(currentElfCalories, maxCalories);
+    calories.push(currentElfCalories);
     currentElfCalories = 0;
   } else {
     const calories = parseInt(line, 10);
     currentElfCalories += calories;
   }
 }
+calories.push(currentElfCalories);
+calories.sort((a, b) => b - a);
 
-console.log(maxCalories);
+const [top1, top2, top3] = calories;
+console.log(top1, top1 + top2 + top3);
